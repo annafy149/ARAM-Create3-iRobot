bfbd8057dda7c1dd2ab4bcaabca114a7f8f1da01
Annafy Hossain <ah3732@drexel.edu>
2023-05-24 11:54:19 -0400

    updated minimum features

--- Maze_solve.py
+++ Maze_solve.py
@@ -1,7 +1,7 @@
 from irobot_edu_sdk.backend.bluetooth import Bluetooth
 from irobot_edu_sdk.robots import event, hand_over, Color, Robot, Root, Create3
 from irobot_edu_sdk.music import Note
-#robot = Root(Bluetooth())git
+#robot = Root(Bluetooth())
 robot = Create3(Bluetooth())
 speed = 15
 status = 'no bump'
@@ -58,5 +58,9 @@ async def play(robot):
             await backoff_left(robot)
         if status == 'no bump':
             await forward(robot)
+            pos = await robot.get_position()
+            print(pos.x / 10, pos.y / 10)
+            await robot.navigate_to(-90, 30)
 
 robot.play()
+
