1879bcb5a73bb39ef6c32add0080f90bfb58b6c1
RafiYanoff1 <ry339@drexel.edu>
2023-05-24 10:37:14 -0400

    removed garbage code

---
+++ ARAM_W7_Rafi.py
@@ -0,0 +1,54 @@
+from irobot_edu_sdk.backend.bluetooth import Bluetooth
+from irobot_edu_sdk.robots import event, hand_over, Color, Robot, Root, Create3
+from irobot_edu_sdk.music import Note
+robot = Create3(Bluetooth())
+speed = 12
+#userChoice = input("select item (1-5): \n\n")
+#input is not supported with irobot
+
+
+#prereq variables
+userChoice = 1
+box_dimension = a = 100
+i = 0
+
+#set up coordinates for the box
+item1 = (a, a)
+item2 = (-a, a)
+item3 = (a, -a)
+item4 = (-a, -a)
+#dictionary of item names and their locations
+inventory = {1: item1, 2: item2, 3: item3, 4: item4}
+target = inventory[int(userChoice)]
+
+#checks if robot is a certain distance from wall
+#unused
+async def check(sensors,num):
+    return sensors[num] > 150
+
+#stops robot when bumped
+
+@event(robot.when_bumped,[True, True])
+async def bumped_robot():
+    await robot.stop()
+    await robot.turn_right(90)
+    await robot.move(50)
+    while i <= len(inventory):
+        #navigates to the inv item from dictionary
+        await robot.navigate_to(inventory[i][0], inventory[i][1])
+        i ++ 1
+
+#main code 
+@event(robot.when_play)
+async def play(robot):
+    #test case to go through each inv item
+    while i <= len(inventory):
+        #navigates to the inv item from dictionary
+        await robot.navigate_to(inventory[i][0], inventory[i][1])
+        i ++ 1
+        #initializes sensor check
+        #not breaking the navigate command, doing weird turns at the corners
+        #only checking once for each sensor
+        
+
+robot.play()
